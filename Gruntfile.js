4d9d7b8c0b62545b83523b10978efa24c6e99a78
Chuck Conway <charles@cconway.com>
2014-02-18 16:18:24 -0800

    Changes to the Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,14 @@ module.exports = function(grunt){
                 files: { '<%= compile_dir %>/assets/<%= pkg.name %>-<%= pkg.version %>.css' : ['<%= app_files.less %>']}
             }
         },
-        /**
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  This is the jsmin task secion. It includes uglifying local and dependencies and wrapping local javascript in a self executing method.
+//  compiled js is saved to bin/assets/
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       /**
          * Minify the sources!
          */
         uglify: {
@@ -206,6 +213,51 @@ module.exports = function(grunt){
                 },
                 src: [ '<%= app_files.ctpl %>' ],
                 dest: '<%= build_dir %>/assets/templates-common-<%= pkg.version %>.js'
+            }
+        },
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                              End of JSmin Task
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /**
+         * The `index` task compiles the `index.html` file as a Grunt template. CSS
+         * and JS files co-exist here but they get split apart later.
+         */
+        index: {
+
+            /**
+             * During development, we don't want to have wait for compilation,
+             * concatenation, minification, etc. So to avoid these steps, we simply
+             * add all script files directly to the `<head>` of `index.html`. The
+             * `src` property contains the list of included files.
+             */
+            build: {
+                dir: '<%= build_dir %>',
+                src: [
+                    '<%= vendor_files.js %>',
+                    '<%= build_dir %>/src/client/**/*.js',
+                    '<%= html2js.common.dest %>',
+                    '<%= html2js.app.dest %>',
+                    '<%= vendor_files.css %>',
+                    //'<%= recess.build.dest %>'
+                ]
+            },
+
+            /**
+             * When it is time to have a completely compiled application, we can
+             * alter the above to include only a single JavaScript and a single CSS
+             * file. Now we're back!
+             */
+            compile: {
+                dir: '<%= compile_dir %>',
+                src: [
+                    '<%= concat.compile_js.dest %>',
+                    '<%= vendor_files.css %>',
+                    '<%= recess.compile.dest %>'
+                ]
             }
         }
     };
